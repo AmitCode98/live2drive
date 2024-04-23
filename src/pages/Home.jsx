7dbfc12d5e3f02e9617bed54aad38a37bad4316a
Amit Gorai <amit.gorai16@gmail.com>
2024-04-23 21:57:31 +0530

    test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import HeroSection from "../sections/HeroSection";
 import AboutUsSection from "../sections/AboutUsSection";
-import KeyFeatures from "../sections/KeyFeatures";
+// import KeyFeatures from "../sections/KeyFeatures";
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <AboutUsSection />
-      <KeyFeatures/>
+      {/* <KeyFeatures/> */}
     </>
   );
 };
